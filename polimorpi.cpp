c62ccbd609200240cf885a38409fc5861d602292
ardiapri <ardiapri882@gmail.com>
2024-05-28 17:48:20 +0700

    polipormi

---
+++ polimorpi.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// nama repositeroti : paradigmaoop2_0023
+// minimal commit 15
+
+class seseorang {
+public:
+    virtual void pesan() = 0;
+    // virtual void pesan(){
+    //       cout<<"pesan dari seseorang"<<endl;
+    //}
+
+    
+};
+
+
