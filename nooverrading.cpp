1a661c2232fc660782682c058ea8315fc92b89a2
ardiapri <ardiapri882@gmail.com>
2024-05-28 18:41:07 +0700

    nooverrading

---
+++ nooverrading.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+class baseclass {
+    public:
+       virtual void perkenalan() {
+          cout << "hallo saya funtion dari base class";
+
+       }
+};
